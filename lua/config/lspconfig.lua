ed3195a1c88e66c278f7c31d1a447269e654c077
ssa <syef.sadman@riiico.com>
2025-02-18 15:02:15 +0100

    changed colorscheme and removed some packages

---
+++ lua/config/lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- Disable eslint formatting as it's slow and timing out on big projects
+    -- taken from
+    -- https://github.com/LazyVim/LazyVim/pull/4225/files
+    setup = {
+      eslint = function()
+        return
+      end,
+    },
+  },
+}
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
   { "sainnhe/gruvbox-material" },
+  { "Everblush/nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "tiagovla/tokyodark.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "retrobox",
+      colorscheme = "tokyodark",
     },
   },
 }
